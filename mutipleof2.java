9a324828a0dd38a8542a52a061185833ff58861a
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-25 21:23:34 +0530

    Add files via upload

---
+++ mutipleof2.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Scanner;
+public class mutipleof2 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner (System.in);
+		System.out.println("the value of i =");
+		int i=scan.nextInt();
+		System.out.println("the value of j =");
+		int j=scan.nextInt();
+		int mul=i*j;
+		System.out.println("The Multiplication of i and j is ="+mul);
+		
+	}
+
+}
