9f592c4c512fc9ec97fcb24414a79ae35c0fc3ce
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-25 20:46:26 +0530

    Add files via upload

---
+++ additioncloselabbook.java
@@ -0,0 +1,15 @@
+package src;
+
+public class additioncloselabbook {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers:");
+		int first = 100;
+		int second = 200;
+		System.out.println(first+" "+second);
+		int sum=first+second;
+		System.out.println("the addition of two number is"+sum);
+		
+	}
+
+}
