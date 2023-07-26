467a88e504fecb50e719d67e22b0e4f909c588b2
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-26 15:13:22 +0530

    Add files via upload

---
+++ closelab7.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.Scanner;
+public class closelab7 {
+
+	public static void main(String[] args) {
+		char ch ='i';
+		
+		
+		if(ch =='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		    System.out.println(ch+ " is vowel");
+        else
+			System.out.println(ch+ " is consonant");
+		    	 
+		
+		
+		
+		
+		
+		
+	}
+
+}
