888dbd6854ef2ec69de5b97daab3d47e8bb46b38
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-26 14:54:16 +0530

    Add files via upload

---
+++ ASCIIVALUE.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.Scanner;
+public class ASCIIVALUE {
+
+	public static void main(String[] args) {
+		char ch = 'h';
+		int ascii = ch;
+		int castAscii = (int) ch;
+		System.out.println("The ASCII value of " + ch + " is " +ascii);
+		System.out.println("The ASCII value of " + ch + " is " +castAscii);
+		
+		
+	}
+
+}
