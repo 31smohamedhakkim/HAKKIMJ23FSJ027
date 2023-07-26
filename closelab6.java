447c2c47f9f72523802255feb0a4d055bde5bd9c
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-26 15:01:02 +0530

    Add files via upload

---
+++ closelab6.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Scanner;
+public class closelab6 {
+
+	public static void main(String[] args) {
+		int dividened =25,divisor =4;
+		int quotient = dividened/divisor;
+		int reminder = dividened%divisor;
+		System.out.println("Quotient "+quotient);
+		System.out.println("Remainder "+reminder);
+		
+		
+		
+		
+	}
+
+}
