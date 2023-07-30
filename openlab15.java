3a352411ac49b5ed9e841be8cb9be485cdf5ce53
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-30 12:04:35 +0530

    Add files via upload

---
+++ openlab15.java
@@ -0,0 +1,44 @@
+package src;
+
+import java.util.Scanner;
+public class openlab15 {
+
+	public static void main(String args[]) {
+		int tech =1;
+		
+		switch(tech) {
+		case 1:{
+			System.out.println("java");
+			break;
+		}
+		case 2:{
+			System.out.println("ES6");
+			break;
+		}
+		default:{
+			System.out.println("Not listed");
+		}	
+			
+		}
+			
+		}
+			
+		
+		
+		
+		
+		
+	
+		
+	
+				
+		
+		
+						
+		
+		
+		
+		
+	}
+
+
