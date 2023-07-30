0e8924c27b40c214029e19234c51bc84a1836b3c
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-30 11:31:10 +0530

    Add files via upload

---
+++ openlab12.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+public class openlab12 {
+
+	public static void main(String[] args) {
+	for (int i=1;i<=5;i++) {
+		   for(int j=1;j<=1;j++) {
+				 System.out.print("* ");
+			}
+		     System.out.println();
+		}
+		
+	
+				
+		
+		
+						
+		
+		
+		
+		
+	}
+
+}
