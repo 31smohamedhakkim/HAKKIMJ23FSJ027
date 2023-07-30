e7e6a78c724b70694f34a66ce979ba9de93e47e3
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-07-30 11:51:08 +0530

    Add files via upload

---
+++ openlab14.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Scanner;
+public class openlab14 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		do {
+			System.out.print("The value of x:"+x);
+			x++;
+			System.out.print("\n");
+		}
+		while(x<1000);
+			
+		}
+		
+		
+		
+		
+	
+		
+	
+				
+		
+		
+						
+		
+		
+		
+		
+	}
+
+
