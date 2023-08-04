bfc0fe8fb7ca5b33cc5615a88d10d182f8073682
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 19:55:36 +0530

    Add files via upload

--- openlab17.java
+++ openlab17.java
@@ -5,12 +5,10 @@ public class openlab17 {
 
 	public static void main(String args[]) {
 		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter the length of rectangle:");
-		double length = scan.nextDouble();
-		System.out.println("Enter the width of the rectangle");
-		double width = scan.nextDouble();
-		double area=length*width;
-		System.out.println("Area of a rectangle is"+area);
+		System.out.println("Enter the area of the sides");
+		double side=scan.nextDouble();
+		double area=side*side;
+		System.out.println("The area of the square is "+area);
 		
 	
 	
