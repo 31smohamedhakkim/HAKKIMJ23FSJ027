e6da8dff6a81673dff79882a608061e387d0f80b
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 20:17:30 +0530

    Add files via upload

---
+++ openlab20.java
@@ -0,0 +1,95 @@
+package src;
+
+import java.util.Scanner;
+public class openlab20 {
+
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the radius od the circle");
+		double radius=scan.nextDouble();
+		double area=Math.PI*radius*radius;
+		System.out.println("The area of the circle is "+area);
+		double circumference = 2*Math.PI*radius;
+		System.out.println("The circumference of the circle is "+circumference);
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
