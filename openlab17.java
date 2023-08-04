57421eddc431adb39e8c32aeceea58cdf21fb4b6
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 19:49:32 +0530

    Add files via upload

---
+++ openlab17.java
@@ -0,0 +1,92 @@
+package src;
+
+import java.util.Scanner;
+public class openlab17 {
+
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the length of rectangle:");
+		double length = scan.nextDouble();
+		System.out.println("Enter the width of the rectangle");
+		double width = scan.nextDouble();
+		double area=length*width;
+		System.out.println("Area of a rectangle is"+area);
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
