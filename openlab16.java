56bf4248e8979656df18c6d15abb3845ed566fc3
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 20:09:00 +0530

    Add files via upload

---
+++ openlab16.java
@@ -0,0 +1,94 @@
+package src;
+
+import java.util.Scanner;
+public class openlab16 {
+
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the length of the triangle");
+		double length=scan.nextDouble();
+		System.out.println("Enter the width of the triangle");
+		double width=scan.nextDouble();
+		double area =length*width;
+		System.out.println("The area of the rectangle is"+area);
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
