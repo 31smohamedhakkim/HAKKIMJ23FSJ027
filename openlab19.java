98cce676e1c744d89dbe0ebfcc3335bdef1dc5da
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 20:05:33 +0530

    Add files via upload

---
+++ openlab19.java
@@ -0,0 +1,94 @@
+package src;
+
+import java.util.Scanner;
+public class openlab19 {
+
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the base of the triangle");
+		double base=scan.nextDouble();
+		System.out.println("Enter the height of the triangle");
+		double height=scan.nextDouble();
+		double area =(base*height)/2;
+		System.out.println("The area of the triangle is"+area);
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
