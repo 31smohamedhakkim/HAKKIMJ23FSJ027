e109c39c304ff4607af9876ff0185f23d1da8c14
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-04 20:36:18 +0530

    Add files via upload

---
+++ openlab21.java
@@ -0,0 +1,103 @@
+package src;
+
+import java.util.Scanner;
+public class openlab21 {
+
+	public static void main(String args[]) {
+		int num=0;
+		int reversenum=0;
+		System.out.println("Enter the number and press enter");
+		Scanner in=new Scanner(System.in);
+		num = in.nextInt();
+		while(num !=0)
+			
+		{	
+			reversenum = reversenum*10;
+			reversenum = reversenum + num%10;
+			num =num/10;
+		}
+		System.out.println("Reverse of the input number is "+reversenum);
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
