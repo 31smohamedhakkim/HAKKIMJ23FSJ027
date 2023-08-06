75ac7896182875c238d7cc27ca61790357b0e131
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-06 17:42:31 +0530

    Add files via upload

---
+++ openlab25.java
@@ -0,0 +1,185 @@
+package src;
+
+import java.util.Scanner;
+public class openlab25 {
+
+	public static void main(String args[]) {
+		Scanner scan =new Scanner (System.in);
+		int[] array =new int[10];
+		int sum = 0;
+		System.out.println("Enter the array elements");
+		for (int i =0;i<10;i++)
+		{
+			array[i] = scan.nextInt();
+		}
+		for (int num :array)
+		{
+			sum = sum+num;
+		}
+		System.out.println("Sum of the array elements is "+sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
