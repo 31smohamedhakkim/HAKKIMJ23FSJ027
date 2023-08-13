a66cb4f402cf72ab71830dae223aeca6009e55fd
31smohamedhakkim <138500756+31smohamedhakkim@users.noreply.github.com>
2023-08-13 20:35:51 +0530

    Add files via upload

---
+++ closelabbook4.java
@@ -0,0 +1,209 @@
+package src;
+
+import java.util.Scanner;
+public class closelabbook4{
+	
+	
+
+	public static void main(String args[]) {
+		int t;
+		boolean prime =true;
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter the number =");
+		int num = scan.nextInt();
+		scan.close();
+		for (int i=2;i<=num/2;i++)
+		{
+			t=num%i;
+			if (t ==0) {
+				prime =false;
+		        break;
+			}
+		}
+		if(prime)
+			System.out.println(num+" is a prime number");
+		else
+			System.out.println(num+" is not a prime number ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
+			
+				
+			
+		
+			
+			
+		
+			
+		
+		
+						
+		
+		
+		
+		
+	
+
+
